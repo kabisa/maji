20e09f889df222b7fec3c194aa1be27f0508832d
Pascal Widdershoven <pascal@kabisa.nl>
2015-02-26 09:24:32 +0100

    Switch from gaze to chokidar for livereload file watching
    Gaze has quite a few issues with changes not being picked up. Chokidar
    is the new hotness.

--- project_template/script/livereload.js
+++ project_template/script/livereload.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
-var tinylr = require('tiny-lr')
-var gaze = require('gaze');
+var tinylr = require('tiny-lr');
+var chokidar = require('chokidar');
 
 var lr = new tinylr.Server
 lr.listen(35729, function (err) { if(err) { throw err } });
 
-gaze('dist/**/*', function(err, watcher) {
- this.on('all', function() {
-   console.log('Reloading browser...')
-   lr.changed({body: {files: ['dummy']}})
- });
+var watcher = chokidar.watch(process.argv[2]);
+watcher.on('ready', function(){
+  watcher.on('all', function() {
+    console.log('Reloading browser...')
+    lr.changed({body: {files: ['dummy']}})
+  });
 });
