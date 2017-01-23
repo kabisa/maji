6f242082bd91a46bcc02161d78d3cd7f6f582936
Roel Nieskens <roel@pixelambacht.nl>
2017-01-23 09:17:44 +0100

    Fix tests and SCSS linter errors

--- project_template/config/iconfont.scss.js
+++ project_template/config/iconfont.scss.js
@@ -1,44 +1,45 @@
 @font-face {
-  font-family: '<%= fontName %>';
-  src: url('<%= fontPath + fontName %>.eot');
-  src: url('<%= fontPath + fontName %>.eot?#iefix') format('eot'),
+    font-family: '<%= fontName %>';
+    src: url('<%= fontPath + fontName %>.eot');
+    src: url('<%= fontPath + fontName %>.eot?#iefix') format('eot'),
     url('<%= fontPath + fontName %>.woff') format('woff'),
     url('<%= fontPath + fontName %>.ttf') format('truetype'),
     url('<%= fontPath + fontName %>.svg#<%= fontName %>') format('svg');
 }
 
 %icon {
-  font-family: '<%= fontName %>';
-  font-smoothing: antialiased;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: normal;
-  text-decoration: none;
-  text-transform: none;
-  text-rendering: auto;
+    font-family: '<%= fontName %>';
+    font-smoothing: antialiased;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-style: normal;
+    font-variant: normal;
+    font-weight: normal;
+    text-decoration: none;
+    text-transform: none;
+    text-rendering: auto;
+    user-select: none; // Avoid text being selectable
 }
 
 @function icon-char($filename) {
-  $char: '';
-<% _.each(glyphs, function(glyph) { %>
-  @if $filename == <%= glyph.fileName %> {
-    $char: '\<%= glyph.codePoint %>';
-  }<% }); %>
-
-  @return $char;
+    $char: '';
+    <% _.each(glyphs, function(glyph) { %>
+    @if $filename == <%= glyph.fileName %> {
+        $char: '\<%= glyph.codePoint %>';
+    }
+    <% }); %>
+    @return $char;
 }
 
 @mixin icon($filename, $insert: before) {
-  &:#{$insert} {
-    @extend %icon;
-    content: icon-char($filename);
-  }
+    &:#{$insert} {
+        @extend %icon;
+        content: icon-char($filename);
+    }
 }
 
 <% _.each(glyphs, function(glyph) { %>%icon-<%= glyph.fileName %> {
-  @include icon(<%= glyph.fileName %>);
+    @include icon(<%= glyph.fileName %>);
 }
 
 <% }); %>
