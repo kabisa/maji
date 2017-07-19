9f9585d5f63c7a99f1696281716eaa8ab8447b04
Rui Salgado <ruivfsalgado@gmail.com>
2017-07-19 21:41:36 +0100

    Bump up stylelint and ignore :global pseudo class

--- project_template/stylelint.config.js
+++ project_template/stylelint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    indentation: 4
+    indentation: 4,
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"]
+      }
+    ]
   }
 };
