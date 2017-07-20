d5b48351715548961dd2ffe53328801f600e67f3
Rui Salgado <ruivfsalgado@gmail.com>
2017-07-20 13:51:09 +0100

    Add stylelint-no-unsupported-browser-features plugin
    Making use of existing browsers list.
    As recommended by the plugin, compliance failures 
    are logged at the “warning” level.

--- project_template/stylelint.config.js
+++ project_template/stylelint.config.js
@@ -1,11 +1,21 @@
+const supportedBrowsers = require("./config/supported-browsers");
+
 module.exports = {
   extends: "stylelint-config-standard",
+  plugins: ["stylelint-no-unsupported-browser-features"],
   rules: {
     indentation: 4,
     "selector-pseudo-class-no-unknown": [
       true,
       {
         ignorePseudoClasses: ["global"]
+      }
+    ],
+    "plugin/no-unsupported-browser-features": [
+      true,
+      {
+        browsers: supportedBrowsers,
+        severity: "warning"
       }
     ]
   }
