55aa57157dc7f8304736391409508c7b171131da
Rui Salgado <ruivfsalgado@gmail.com>
2017-07-21 16:57:54 +0100

    Prevent “false successes” in e2e tests
    Maybe ditching selenium server wouldn’t be a bad 
    idea after all…

--- project_template/script/test-e2e.js
+++ project_template/script/test-e2e.js
@@ -1,9 +1,24 @@
 #!/usr/bin/env node
 
-const spawn = require("child_process").spawn;
+const child_process = require("child_process");
+
+const hasJava = function() {
+  return child_process.spawnSync("java", ["-version"]).status == 0;
+};
+
+// workaround for issue https://github.com/nightwatchjs/nightwatch/issues/1454
+// prevents false positives.
+// Can be skipped if selenium server is disabled in nightwatch
+if (!hasJava()) {
+  const colorRed = "\x1b[31m";
+  const colorReset = "\x1b[0m";
+  console.error(`${colorRed}JAVA not found. Exiting${colorReset}`);
+  process.exit(1);
+}
+
 const server = require("./dev-server.js");
 
-const testRunner = spawn(
+const testRunner = child_process.spawn(
   "./node_modules/.bin/nightwatch",
   ["-c", "config/nightwatch.js"],
   { stdio: "inherit", env: process.env }
@@ -15,6 +30,7 @@ testRunner.on("exit", function(code) {
 });
 
 testRunner.on("error", function(err) {
+  console.error(err);
   server.close();
-  throw err;
+  process.exit(1);
 });
