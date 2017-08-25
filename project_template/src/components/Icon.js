72b5684707155a48007c53dd528762e9053d0083
Roel Nieskens <roel@pixelambacht.nl>
2017-08-25 15:54:50 +0200

    Prevent ID in inlined SVG to avoid duplicate IDs

--- project_template/src/components/Icon.js
+++ project_template/src/components/Icon.js
@@ -2,5 +2,5 @@ import { h } from "preact";
 
 export default props => {
   const icon = require(`src/styles/icons/${props.id}.svg`).default;
-  return <svg {...props}><use xlinkHref={icon.url} /></svg>;
+  return <svg><use xlinkHref={icon.url} /></svg>;
 };
