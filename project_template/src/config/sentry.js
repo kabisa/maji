3c3aadfc2218e9a048b20fcee983726f99237499
Pascal Widdershoven <pascal@kabisa.nl>
2017-09-01 15:21:47 +0200

    Fix broken production build due to invalid sentryDsn used as example
    Sentry will raise when the configured DSN is invalid.
    So instead of adding a sample value in the config, we console.warn in
    production if no DSN is configured.

--- project_template/src/config/sentry.js
+++ project_template/src/config/sentry.js
@@ -9,6 +9,13 @@ if (Settings.sentryDsn) {
       revision: __BUILD_IDENTIFIER__
     }
   }).install();
+} else {
+  if (process.env.NODE_ENV === "production") {
+    /* eslint-disable no-console */
+    console.warn(`Sentry exception tracking is NOT enabled. Either add a 'sentryDsn' property
+    in 'settings.production.json' or remove the Sentry import in index.js to silence this warning.`);
+    /* eslint-enable no-console */
+  }
 }
 
 export default Raven;
