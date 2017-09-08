98747b019ec2e1c7efeaa5bce12d3d577b7cfe38
Matthijs Groen <matthijs.groen@gmail.com>
2017-09-08 09:48:34 +0200

    Provide name for components
    This helps with using the React dev tools to inspect elements
    from the browser.

--- project_template/src/components/Icon.js
+++ project_template/src/components/Icon.js
@@ -1,6 +1,8 @@
 import { h } from "preact";
 
-export default ({ icon, ...props }) => {
+const Icon = ({ icon, ...props }) => {
   const iconAsset = require(`src/assets/icons/${icon}.svg`).default;
   return <svg {...props}><use xlinkHref={iconAsset.url} /></svg>;
 };
+
+export default Icon;
