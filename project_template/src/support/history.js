b25cce66d088cc89098704acec9df2267c745d93
Leon de Rijke <leon@leonderijke.nl>
2017-12-18 16:42:00 +0100

    Remove history keeping

--- project_template/src/support/history.js
+++ project_template/src/support/history.js
@@ -1,13 +1,3 @@
 import createHistory from "history/createHashHistory";
 
-let previous = null;
 export const history = createHistory();
-
-history.listen(function(location, action) {
-  previous = action === "PUSH" ? location : null;
-});
-
-export function goBack(location) {
-  if (previous) history.goBack();
-  else history.replace(location);
-}
