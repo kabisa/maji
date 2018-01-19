f87288f60022e340102f01b918d6b7ddc62c2364
Matthijs Groen <matthijs.groen@gmail.com>
2018-01-19 16:02:48 +0100

    Prettier 💅

--- script/create-package-json.js
+++ script/create-package-json.js
@@ -2,16 +2,16 @@
 
 const maji_package = require("../package.json");
 const project_package = require("../project_template/package.json");
-const [target, appName] = process.argv.slice(2)
+const [target, appName] = process.argv.slice(2);
 
 project_package.name = appName;
-project_package.dependencies['maji'] = maji_package.version
+project_package.dependencies["maji"] = maji_package.version;
 
 const content = JSON.stringify(project_package, null, 2);
 
-const fs = require('fs');
-fs.writeFile(""+ target + "/package.json", content, function(err) {
-  if(err) {
-    process.exit(1)
+const fs = require("fs");
+fs.writeFile("" + target + "/package.json", content, function(err) {
+  if (err) {
+    process.exit(1);
   }
-})
+});
